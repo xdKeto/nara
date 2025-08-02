30cd29ff1466fa71b9eedf6a35c151ec7e5974cc
Yestoya L Minggus <yestoya.minggus@gmail.com>
2025-08-02 17:07:57 +0700

    fuck

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -170,8 +170,8 @@ const Home = () => {
 
       {/* Modal Photo Viewer */}
       {viewerOpen && selectedItem && (
-        <div className="fixed inset-0 bg-black/95 flex flex-col md:flex-row items-center justify-start pt-32 md:pt-0 md:justify-center z-50 p-4 md:p-16 gap-8 lg:gap-16" onClick={closeViewer}>
-          <div className="w-full lg:w-3/8 md:py-8 lg:pe-32">
+        <div className="fixed inset-0 bg-black/95 flex flex-col md:flex-row items-center justify-start pt-32 md:pt-0 md:justify-center z-50 p-4 md:p-16 gap-8 xl:gap-16" onClick={closeViewer}>
+          <div className="w-full lg:w-3/8 md:py-8 xl:pe-32">
             <h1 className="text-white text-xl md:text-5xl font-bold"> {selectedItem.title} </h1>
             <h1 className="text-white text-sm md:text-xl font-semibold"> {selectedItem.description} </h1>
             <h1 className="text-white text-xs  md:text-xl mt-8 font-medium"> {selectedItem.madeIn} </h1>
